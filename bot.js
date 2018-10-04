38949ac99bc509f001c58a7f2d105845e50674d5
Dunaas <39232355+Dunaas@users.noreply.github.com>
2018-10-04 15:21:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var tmi = require('tmi.js');
 
 
-var canais = ['dunaaas','diffensive','sirgank','mahriojr','dobrein','noelbh', 'gaules'];
+var canais = ['dunaaas','diffensive','sirgank','mahriojr','dobrein','noel', 'gaules'];
 
 var options = {
     options: {
